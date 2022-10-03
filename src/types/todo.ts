febfce375812ef7be655f7dd778318d6cb0f82cc
Nurlybek316728 <aigerimutebek@gmail.com>
2022-10-03 18:57:04 +0600

    todos

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,5 +1,31 @@
-interface TodoState{
-    todos:[];
-    loading:boolean;
-    error: null | string
+export interface TodoState {
+    todos: [];
+    loading: boolean;
+    error: null | string;
+    page: number;
+    limit: number;
 }
+
+export enum TodoActionTypes {
+    FETCH_TODOS = "FETCH_TODOS",
+    FETCH_TODOS_SUCCESS = "FETCH_TODOS_SUCCESS",
+    FETCH_TODOS_ERROR = "FETCH_TODOS_ERROR",
+    SET_TODO_PAGE = 'SET_TODO_PAGE'
+}
+
+interface FetchTodoAction {
+    type: TodoActionTypes.FETCH_TODOS
+}
+interface FetchTodoSuccesAction {
+    type: TodoActionTypes.FETCH_TODOS_SUCCESS;
+    payload: any[];
+}
+interface FetchTodoErrorAction {
+    type: TodoActionTypes.FETCH_TODOS_ERROR;
+    payload: string;
+}
+interface SetTodoPage {
+    type: TodoActionTypes.SET_TODO_PAGE
+}
+
+export type TodoAction = FetchTodoAction | FetchTodoErrorAction | FetchTodoSuccesAction | SetTodoPage
